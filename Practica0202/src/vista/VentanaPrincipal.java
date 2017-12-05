d2853f21593d91c6cdce1e1f9e9e3faa8366bcf2
Jorge Pizarro <Jorge Pizarro@172.16.217.211>
2017-12-05 10:38:11 -0500

    Primera Versión de Práctica2 Queda Creado los paquetes y Clases Principal - Ventana Principal - Gestión dato - Evento Principal Se Implementa intento de internalización xD

--- Practica0202/src/vista/VentanaPrincipal.java
+++ Practica0202/src/vista/VentanaPrincipal.java
@@ -5,10 +5,42 @@
  */
 package vista;
 
+import controlador.GestionDato;
+import java.awt.HeadlessException;
+import java.util.List;
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
 /**
  *
  * @author Jorge Pizarro
  */
-public class VentanaPrincipal {
+public class VentanaPrincipal extends JFrame{
+    
+    private JDesktopPane    escritorio;
+    private JMenuBar        barraMenu;
+    private List<JMenu>     menuList;
+    private List<JMenuItem> itemMenuList;
+    private GestionDato     gD;
+
+    public VentanaPrincipal(GestionDato gD, String title) throws HeadlessException {
+        super(title);
+        this.gD = gD;
+        this.iniciaComponentes();
+        this.setSize(600, 500);
+        this.setDefaultCloseOperation(3);
+        
+    }
+    
+    public void iniciaComponentes(){
+        this.asignarIdioma();
+    }
+    
+    public void asignarIdioma(){
+        
+    }
     
 }
